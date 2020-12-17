bd8a2d7af745da6f615a08cbf24a98fbf7c09333
FrancescaPalazzetti <S1094008@studenti.univpm.it>
2020-12-17 22:47:27 +0100

    Filtri aggiuntivi

--- WeatherApp/src/main/java/com/project/WeatherApp/utils/FilterStats.java
+++ WeatherApp/src/main/java/com/project/WeatherApp/utils/FilterStats.java
@@ -1,10 +1,12 @@
 package com.project.WeatherApp.utils;
 
+import java.util.ArrayList;
+
 import org.json.JSONArray;
 
 public interface FilterStats {
 	
-	public abstract JSONArray oneDay(String name1, String name2, String name3, String value);
-	public abstract JSONArray fiveDay(String name1, String name2, String name3, String value);
+	public abstract JSONArray oneDay(ArrayList<String> cities, String value);
+	public abstract JSONArray fiveDay(ArrayList<String> cities, String value);
 
 }
